f826a57f90ff45f8f36f499a3fcfbaa64f6c9d44
Ayine-nongre <eugeneatinbire@gmail.com>
2024-05-16 13:15:04 +0000

    Adding expiry to token

--- middleware/tokenizer.js
+++ middleware/tokenizer.js
@@ -7,7 +7,7 @@ dotenv.config()
 // create a token to authenticate users
 export const createToken = async (user, res, statusCode) => {
     // encode user id and role in token
-    const token = jwt.sign({email: user.email, role: user.role, status: user.status }, process.env.PRIVATEKEY)
+    const token = jwt.sign({email: user.email, role: user.role, status: user.status }, process.env.PRIVATEKEY, { expiresIn: '1h' })
 
     if (statusCode === 200) {
          res.status(statusCode).json({
